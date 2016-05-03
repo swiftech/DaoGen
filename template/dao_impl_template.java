a2066b3b4c0632b2ac3c1bf6187666ff042c3571
swiftech <awsksupport@gmail.com>
2016-05-03 21:18:33 +0800

    bug

--- template/dao_impl_template.java
+++ template/dao_impl_template.java
@@ -3,7 +3,7 @@ package <%-dao_pkg_name-%>.impl;
 import org.springframework.stereotype.Repository;
 import <%-base_dao_impl-%>;
 import <%-entity_pkg_name-%>.<%-entity_name-%>;
-import <%-dao_package_name-%>.<%-dao_name-%>;
+import <%-dao_pkg_name-%>.<%-dao_name-%>;
 /**
  * <%=dao_desc%>
  */
